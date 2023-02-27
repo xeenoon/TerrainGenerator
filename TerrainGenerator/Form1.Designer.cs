392bb6ffee92552ac18188bef3f20473c1eb2f9e
Chris <ccw100asdf@gmail.com>
2023-02-28 10:43:35 +1100

    Changed default zoom

--- TerrainGenerator/Form1.Designer.cs
+++ TerrainGenerator/Form1.Designer.cs
@@ -55,7 +55,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(46, 23);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "8";
+            this.textBox1.Text = "9";
             // 
             // textBox2
             // 
